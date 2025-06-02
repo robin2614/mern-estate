41b76cfcf3860ca9d424c50d4404df012ce84b92
robin2614 <mehulmakwana2614@gmail.com>
2025-06-02 17:00:52 +0530

    create pages and routes

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='bg-slate-200 shadow-md'>
+        <div className='flex jusify-between items-center max-w-6xl mx-auto p-3'>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'>Robin</span>
+            <span className='text-slate-700'>Estate</span>
+        </h1>
+        <form className='bg-slate-100 p-3 rounded-lg    '>
+            <input type="text" placeholder='Search...' 
+            className='bg-transparent'/>
+        </form>
+        </div>
+    </header>
+  )
+}
