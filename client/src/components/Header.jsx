343930c4c95bbaef8357ef6bdb6a20b2ff16cd53
robin2614 <mehulmakwana2614@gmail.com>
2025-06-04 19:25:15 +0530

    create server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,15 +3,15 @@ import {Link} from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className='bg-slate-200 shadow-md'>
+    <header className='bg-slate-200 shadow-md flex gap-5'>
         <div className='flex jusify-between items-center max-w-6xl mx-auto p-3'>
         <Link to="/">
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Robin</span>
-            <span className='text-slate-700'>Estate</span>
+            <span className='text-slate-500 '>Robin</span>
+            <span className='text-slate-700 '>Estate</span>
         </h1>
         </Link>
-        <form className='bg-slate-100 p-3 rounded-lg  flex items-center'>
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
             <input 
              type="text" 
             placeholder='Search...' 
